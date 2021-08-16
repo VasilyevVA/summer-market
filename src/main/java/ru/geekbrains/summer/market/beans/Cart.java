c80eadc432a82bc6026be85a87807d8f681a2725
VasilyevVA <vladislavovix@live.com>
2021-08-16 17:32:02 +0700

    HomeWork - 10

---
+++ src/main/java/ru/geekbrains/summer/market/beans/Cart.java
@@ -0,0 +1,53 @@
+package ru.geekbrains.summer.market.beans;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.geekbrains.summer.market.model.Product;
+import ru.geekbrains.summer.market.services.ProductServices;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+@Data
+public class Cart {
+
+    private final ProductServices services;
+    private Map<Product, Integer> itemsMap;
+    private int totalPrice;
+
+    @PostConstruct
+    public void init() {
+        this.itemsMap = new HashMap<>();
+    }
+
+    public List<String> viewCartProductList() {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<Product, Integer> product : itemsMap.entrySet()) {
+            list.add(product.getKey() + ", " + product.getValue());
+        }
+        return list;
+    }
+
+    public void addProductToCart(Long id) {
+        Product p = getProductById(id);
+
+        itemsMap.compute(p, (product, integer) -> integer  == null ? 1 : ++integer);
+    }
+
+    public void removeProductFromCart(Long id) {
+        Product p = getProductById(id);
+        itemsMap.computeIfPresent(p, (product, integer) -> --integer);
+    }
+
+    public void clear() {
+        itemsMap.clear();
+    }
+
+    private Product getProductById(Long id) {
+        Product p = services.findProductById(id).map(Product::new).get();
+        return p;
+    }
+}
--- src/main/java/ru/geekbrains/summer/market/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/summer/market/controllers/ProductController.java
@@ -1,23 +1,26 @@
 package ru.geekbrains.summer.market.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.summer.market.dto.ProductDto;
+import ru.geekbrains.summer.market.exception_handling.ResourceNotFoundException;
 import ru.geekbrains.summer.market.model.Product;
 import ru.geekbrains.summer.market.repositories.specifications.ProductSpecifications;
 import ru.geekbrains.summer.market.services.ProductServices;
 
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/api/v2/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductServices services;
 
-    //`http://localhost:8189/market/index.html`
+    //`http://localhost:8190/market/index.html`
+    @ApiOperation(value = "receive all products")
     @GetMapping
     public Page<ProductDto> findAllProducts(
             @RequestParam MultiValueMap<String, String> params,
@@ -26,14 +29,19 @@ public class ProductController {
         if (page < 1) {
             page = 1;
         }
+
+        //http://localhost:8190/market/api/v2/products/
         return services.findAll(ProductSpecifications.build(params), page, 5);
     }
 
+    @ApiOperation(value = "receive the product by ID")
     @GetMapping("/{id}")
-    public Product findById(@PathVariable Long id) {
-        return services.findProductById(id).get();
+    public ProductDto findById(@PathVariable Long id) {
+        return services.findProductById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String
+                        .format("id %s not found", id)));
     }
-
+    @ApiOperation(value = "Saving the product")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product saveNewProduct(@RequestBody Product product) {
@@ -41,11 +49,12 @@ public class ProductController {
         return services.saveOrUpdate(product);
     }
 
+    @ApiOperation(value = "Updating the product")
     @PutMapping
     public Product updateProduct(@RequestBody Product product) {
         return services.saveOrUpdate(product);
     }
-
+    @ApiOperation(value = "Remove product by ID")
     @DeleteMapping("/{id}")
     public void deleteProductById(@PathVariable Long id) {
         services.deleteProductById(id);
